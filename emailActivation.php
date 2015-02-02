e2cf180749646791c5fbfd6a30d4755c4d8be9fd
JadSayegh <jad.r.sayegh@gmail.com>
2015-02-02 17:44:07 -0500

    removed deprecated code of emailActivation.php

--- emailActivation.php
+++ emailActivation.php
@@ -4,38 +4,15 @@ $mailerLoaderPath = 'PHPMailer-master/';
 require $mailerLoaderPath.'PHPMailerAutoload.php';
    
 
-function registerUser(){
-    
-    #TEST values
-    $name = 'Jad';
-    $username = 'scummyj';
-    $password = 'onionknight';
-    $email = 'jad.r.sayegh@gmail.com';
-    
-    $email_code = md5($username + microtime());
-    #TEST
-    echo "Email code is: ".$email_code. "\n";
-    
-    $registration = [ 'name'=>$name, 'username'=>$username, 'password'=> $password, 'email'=>$email, 'email_code'=>$email_code, 'activation'=>false];
-    
-    
-    #TODO: check that the username / email is not already in the DB
-    #TODO: put user values in the database
-    #TODO: check that the database entry worked
-    
-    sendActivationEmail($registration);
-    
-}
-
-function sendActivationEmail($registration){
+function sendActivationEmail($username, $email, $email_code){
     
     
     $subject = 'Roomr account activation';
     
     #TODO: change below for different environment
-    $activationLink  = "http://localhost:8888/activate.php?email=".$registration['email']."&email_code=".$registration['email_code'];
+    $activationLink  = "http://localhost:8888/activate.php?email=".$email."&email_code=".$email_code;
         
-    $message = $registration['username'].",\n\n".
+    $message = $username.",\n\n".
         "Thank you for joining the Roomr community!\n".
         "We are dedicated to helping you find roommates you enjoy living with. Please click on the link below to activate your account, and begin meeting potential roommates:\n\n\n"
         
@@ -48,7 +25,7 @@ Cheers,\n
 The Roomr Team
     ";
     
-    email($registration['email'], $subject, $message);
+    email($email, $subject, $message);
 }
 
 #send an email from activation@roomr.com
