4b7bf3c84e2202282ceebeca12d012cf858940d7
JadSayegh <jad.r.sayegh@gmail.com>
2015-02-02 23:20:16 -0500

    Added email_exists function and account activation functionality

--- activate.php
+++ activate.php
@@ -10,21 +10,28 @@ if(isset($_GET['email'], $_GET['email_code'])){
     $email = trim($_GET['email']);
     
     #check if the email exists
-    if(email_exists($email) == false){
-        #TODO: log error
-    }else if(activate_user($email, $email_code) == false ){
-        #TODO: log error "Problem activating account"
-        
+    if(checkActivation($email, $email_code)){
+        echo '[ERROR]: invalid email/activation code combination';
+    }else{
+        $sql  = "UPDATE USER SET activated=1 WHERE email = " . $email." AND email_code = ".$email_code;     
+        echo 'Activation was successful';
     }
-    
-    #check if they are already activated 
-    
-    
-    
 }else{
     echo 'BAD URL';
     #exit();
 }
 
+
+function checkActivation($email, $email){
+    if(!email_exists($email)){
+        return false;
+    }else{
+        $sql = "SELECT email FROM users WHERE email = '$email' AND email_code = '$email_code'";
+        if($result = $db->query($sql) && $result->num_rows == 1){
+           return true;  
+        }
+        return false;
+    }
+}
 
 ?>
--- user.php
+++ user.php
@@ -8,13 +8,25 @@ function activate_user($email, $email_code){
     return false;
 }
 
+
 #tests: 1) email exists 2) email doesnt exist
 function email_exists($email){
     #TODO: stub
-    echo "\nEmail doesn't exists\n";
-    return false;
+    
+    $sql = "SELECT email FROM users WHERE email = '$email'";
+    if(!$result = $db->query($sql)){
+        die('There was an error running the query [' . $db->error . ']');
+	}else if($result->num_rows == 0){
+        echo "\nEmail doesn't exists\n";
+        return false;
+    
+    }
+    
+    echo "\nEmail exists\n";
+    return true;
 }
 
+#DEPRECATED
 #tests: 1) all is good 2) check that activated is false at the end 3)username already exists 4) email already exists 5) check that there is a email_code identical to the one sent
 function register_user($username, $password, $name, $email){
 
