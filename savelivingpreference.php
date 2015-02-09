2cb01df20b6a53dee3802e1bd7eabeaff21681f5
damien-dg <damiedg@gmail.com>
2015-02-08 21:46:11 -0500

    final changes, request database write

--- savelivingpreference.php
+++ savelivingpreference.php
@@ -1,8 +1,15 @@
 <?php
 header('Content-type: text/html; charset=utf-8');
 include 'dbConnect.php';
-
-		$user_id = '3';
+		
+		session_start();
+		$username = $_SESSION['username'];
+		$sql = "SELECT id from users  WHERE username = '$username'";
+		$result = $db->query($sql) or die('There was an error running the query [' . $db->error . ']');
+		
+		$activateValue = $result->fetch_row();
+		$user_id = $activateValue[0];
+		
 		
        //get form data
        $houseApartment = $_GET['houseApartment'];
@@ -13,8 +20,9 @@ include 'dbConnect.php';
 	   $neighbourhood = $_GET['neighbourhood'];
 	   $roommates = $_GET['roommates'];
        
-	    $update =  mysqli_query($db, "UPDATE users SET houseApartment = '$houseApartment', petFriendly = '$petFriendly', party = '$party', smokingFriendly = '$smokingFriendly', rentPriceRange = '$rentPriceRange', neighbourhood = '$neighbourhood', roommates = '$roommates' WHERE id = $user_id") or die(mysqli_error());
-                
+	    $update =  mysqli_query($db, "UPDATE users SET houseApartment = '$houseApartment', petFriendly = '$petFriendly', party = '$party', smokingFriendly = '$smokingFriendly', rentPriceRange = '$rentPriceRange', neighbourhood = '$neighbourhood', roommates = '$roommates' WHERE id = '$user_id'") or die(mysqli_error());
+        
+		$request = mysqli_query($db, "INSERT INTO requests (userid, neighborhoods, roommates, fees, interests, housed, smoke, pet_friendly) VALUES ('$user_id', '$neighbourhood', '$roommates', '$rentPriceRange', '$party','$houseApartment', '$smokingFriendly','$petFriendly')" ) or die('There was an error running the query [' . $db->error . ']');  
         echo "ok";
              
     ?>
