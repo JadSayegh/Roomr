1b870cd5cb265e4b7101fc13a24c06aa7f6e1b72
JadSayegh <jad.r.sayegh@gmail.com>
2015-02-10 23:57:41 -0500

    fixed bug where $sql request was using a result as an associative array

--- roomrLogin.php
+++ roomrLogin.php
@@ -1,10 +1,37 @@
 <?php
 
 require("dbConnect.php");
-
+    
+    
 	$username = ($_POST['username']);
 	//$email = ($_POST['email']);
 	$password = ($_POST['password']);
+
+	if(checkExistance($db, $username, $password)){
+			$sql = "UPDATE users SET login = 1 WHERE username = '$username'";
+			if(!$result = $db->query($sql)){
+				die('There was an error running the query [' . $db->error . ']');
+			}
+			else{
+				session_start();
+				$sql = "SELECT email FROM users WHERE username = '$username'";
+				$result = $db->query($sql);
+                $result_array = mysql_fetch_assoc($result)
+                $email = $result_array['email'];
+                
+				$_SESSION['username'] = $username;
+				$_SESSION['email'] = $email;
+				$_SESSION['login'] = "1";
+				echo "pass";
+			}
+			
+	}
+	else{
+			echo "USER DOES NOT EXISTS";
+	}
+	
+	$db->close();
+
 
 function checkExistance($database, $user, $password){
 	   
@@ -20,25 +47,5 @@ function checkExistance($database, $user, $password){
 
 	}
 
-	if(checkExistance($db, $username, $password)){
-			$sql = "UPDATE users SET login = 1 WHERE username = '$username'";
-			if(!$result = $db->query($sql)){
-				die('There was an error running the query [' . $db->error . ']');
-			}
-			else{
-				session_start();
-				$sql = "SELECT email FROM users WHERE username = '$username'";
-				$email = $db->query($sql);
-				$_SESSION['username'] = $username;
-				$_SESSION['email'] = $email;
-				$_SESSION['login'] = "1";
-				echo "pass";
-			}
-			
-	}
-	else{
-			echo "USER DOES NOT EXISTS";
-	}
-	
-	$db->close();
+
 ?>
