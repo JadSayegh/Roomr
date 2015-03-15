ab4cf4530ddebf03c3106be421346d01b6ece1bc
damien-dg <damiedg@gmail.com>
2015-03-15 12:10:34 -0400

    basic ui for conversations

--- getConversations.php
+++ getConversations.php
@@ -8,7 +8,15 @@ $senderID = $_SESSION['userid'];
 $sql = "SELECT firstParticipant FROM conversation WHERE secondParticipant = '$senderID' UNION SELECT SecondParticipant FROM conversation WHERE firstParticipant = '$senderID'";
 $result = $db->query($sql) or die('There was an error running the query [' . $db->error . ']');
 
-echo json_encode($result->fetch_all());
+$recipiants = array();
+$i = 0;
+while($row = $result->fetch_row()){
+		$recipiantID = $row[0];
+		$sql = "SELECT id, username, email FROM users WHERE id = '$recipiantID'";
+		$recipiantResult = $db->query($sql);
+		$recipiants[$i] = $recipiantResult->fetch_assoc();
+}
+echo json_encode($recipiants);
 
 
 
